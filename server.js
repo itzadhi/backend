c3eb44ebe8fe61a46f930e8d90e7ef89ef95d42a
adhit7 <adhithyanalan@gmail.com>
2024-03-26 19:12:36 +0530

    log changes

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors({ credentials: true, origin: true }));
+// app.use(cors({ credentials: true, origin: true }));
 app.use(cookieParser());
 
 app.get('/', (req, res) => {
--- utils/generateToken.js
+++ utils/generateToken.js
@@ -10,8 +10,8 @@ const generateToken = (res, userId) => {
   // Set JWT as an HTTP-Only cookie
   res.cookie('jwt', token, {
     httpOnly: false,
-    secure: process.env.NODE_ENV !== 'development', // We have use secure cookies in production
-    sameSite: 'strict', // Prevent CSRF attacks
+    secure: true, // We have use secure cookies in production
+    sameSite: 'none', // Prevent CSRF attacks
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
   });
 };
