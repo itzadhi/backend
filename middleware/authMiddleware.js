bc967e3cd7c8a74b2c897468741f352b89a7aee3
adhit7 <adhithyanalan@gmail.com>
2024-03-26 19:00:09 +0530

    log changes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -6,13 +6,19 @@ import User from '../models/User.js';
 const protect = asyncHandler(async (req, res, next) => {
   let token;
 
+  console.log('111', token);
+
   // Get JWT from the cookie
   token = req.cookies.jwt;
+
+  console.log('233444', token);
 
   if (token) {
     try {
       //Get the User Object
       const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+      console.log('33', decoded);
 
       req.user = await User.findById(decoded.userId).select('-password');
 
